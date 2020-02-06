3d97003873ee2142e7f84adba849da9c2513fa80
itsequal <60673814+itsequal@users.noreply.github.com>
2020-02-06 16:20:38 -0600

    Primer trabajo mejorado
    Primer trabajo simplificado

--- primertrabajo.js
+++ primertrabajo.js
@@ -1,15 +1,18 @@
-var num1 = Math.floor(Math.random() * 20);
-var num2 = Math.floor(Math.random() * 20);
-var num3 = Math.floor(Math.random() * 20);
+let num1 = Math.floor(Math.random() * 20);
+let num2 = Math.floor(Math.random() * 20);
+let num3 = Math.floor(Math.random() * 20);
+
 console.log(num1);
 console.log(num2);
 console.log(num3);
 
-if (num3<num1 && num3<num2)
-  console.log("El numero menor es: ", num3);
+if (num1 < num2)
+  if (num1 < num3)
+    console.log("El numero menor es: ",num1);
+  else
+    console.log("El numero menor es: ",num3);
 else
-if (num2<num1 && num2<num3)
-  console.log("El numero menor es: ", num2);
-else
-if (num1<num2 && num1<num3)
-  console.log("El numero menor es: ", num1);
+if (num2 < num3)
+  console.log("El numero menor es: ",num2);
+  else
+  console.log("El numero menor es: ",num3);
